7f05c04142491026e000bacc942221fd708aeb75
Chad <chad@chella.co.za>
2026-04-21 20:57:11 +0200

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isPages = process.env.GITHUB_PAGES === "true";
+const repo = "chella-quest";
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: isPages ? `/${repo}` : undefined,
+  assetPrefix: isPages ? `/${repo}/` : undefined,
 };
 
 module.exports = nextConfig;
